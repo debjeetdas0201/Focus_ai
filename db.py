d469f08ed9b5dae26a459ab4742d5bb2374f50b9
unknown <nishankparshar@gmail.com>
2024-02-14 12:07:04 +0530

    updated date and prompt

--- db.py
+++ db.py
@@ -21,34 +21,8 @@ EMBEDDINGS_MODEL_DEPLOYMENT_NAME = os.getenv("EMBEDDINGS_MODEL_DEPLOYMENT_NAME")
 OPENWEATHERMAP_API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
 
 # Load documents from the 'docs' directory
-persona = "manager"
-path = 'docs/'+ persona
+persona_li = ["manager", "agent", "cxo"]
 
-documents = []
-for file in os.listdir(path):
-    if file.lower().endswith('.pdf'):
-        input_file_path = os.path.join(path, file)
-        loader = PyPDFLoader(input_file_path)
-        documents.extend(loader.load())
-    elif file.endswith('.docx') or file.endswith('.doc'):
-        doc_path = './docs/' + file
-        loader = Docx2txtLoader(doc_path)
-        documents.extend(loader.load())
-    elif file.endswith('.csv'):
-        print(file)
-        input_file_path = os.path.join(path, file)
-        loader = CSVLoader(input_file_path)
-        documents.extend(loader.load())
-    elif file.endswith('.txt') or file.endswith('.md'):
-        print(file)
-        input_file_path = os.path.join(path, file)
-        loader = TextLoader(input_file_path)
-        documents.extend(loader.load())
-    
-
-# Split documents using the CharacterTextSplitter
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
-chunked_documents = text_splitter.split_documents(documents)
 
 # Initialize OpenAIEmbeddings
 embeddings = OpenAIEmbeddings(
@@ -58,34 +32,64 @@ embeddings = OpenAIEmbeddings(
     openai_api_version=OPENAI_API_VERSION,
     deployment=EMBEDDINGS_MODEL_DEPLOYMENT_NAME
 )
-
 # Batch mechanism to create vector databases with a sleep interval
 batch_size = 15  # You can adjust the batch size as needed
-total_chunks = len(chunked_documents)
-print(total_chunks)
-chunks_processed = 0
-faiss_dbs = []
 
-batch = chunked_documents[chunks_processed: chunks_processed + batch_size]
-final_db = FAISS.from_documents(batch, embeddings)
-chunks_processed += batch_size
-time.sleep(5)
 
-while chunks_processed < total_chunks:
-    # Take a batch of chunks
+for x in persona_li:
+    path = 'docs/'+ x
+    documents = []
+    for file in os.listdir(path):
+        if file.lower().endswith('.pdf'):
+            input_file_path = os.path.join(path, file)
+            loader = PyPDFLoader(input_file_path)
+            documents.extend(loader.load())
+        elif file.endswith('.docx') or file.endswith('.doc'):
+            doc_path = './docs/' + file
+            loader = Docx2txtLoader(doc_path)
+            documents.extend(loader.load())
+        elif file.endswith('.csv'):
+            print(file)
+            input_file_path = os.path.join(path, file)
+            loader = CSVLoader(input_file_path)
+            documents.extend(loader.load())
+        elif file.endswith('.txt') or file.endswith('.md'):
+            print(file)
+            input_file_path = os.path.join(path, file)
+            loader = TextLoader(input_file_path)
+            documents.extend(loader.load())
+        
+
+    # Split documents using the CharacterTextSplitter
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+    chunked_documents = text_splitter.split_documents(documents)
+
+    
+    total_chunks = len(chunked_documents)
+    print(total_chunks)
+    chunks_processed = 0
+    faiss_dbs = []
+
     batch = chunked_documents[chunks_processed: chunks_processed + batch_size]
-
-    # Create vector database for the current batch
-    db_batch = FAISS.from_documents(batch, embeddings)
-    final_db.merge_from(db_batch)
-   
-    # Append the current batch vector database to the list
-
-    # Update the number of processed chunks
+    final_db = FAISS.from_documents(batch, embeddings)
     chunks_processed += batch_size
-    print(chunks_processed)
-    # Add a sleep interval between batches (adjust as needed)
-    time.sleep(10)
+    time.sleep(5)
 
-# Save the final index to a local file
-final_db.save_local("faiss_index/faiss_index_"+persona)
+    while chunks_processed < total_chunks:
+        # Take a batch of chunks
+        batch = chunked_documents[chunks_processed: chunks_processed + batch_size]
+
+        # Create vector database for the current batch
+        db_batch = FAISS.from_documents(batch, embeddings)
+        final_db.merge_from(db_batch)
+    
+        # Append the current batch vector database to the list
+
+        # Update the number of processed chunks
+        chunks_processed += batch_size
+        print(chunks_processed)
+        # Add a sleep interval between batches (adjust as needed)
+        time.sleep(10)
+
+    # Save the final index to a local file
+    final_db.save_local("faiss_index/faiss_index_"+x)
